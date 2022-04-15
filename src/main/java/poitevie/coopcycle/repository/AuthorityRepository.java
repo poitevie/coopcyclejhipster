96a44088df2ddab42a7b9a0891e9cb9a0b0d866a
Eve Poitevin <poitevineve@gmail.com>
2022-04-15 09:59:32 +0200

    clean

--- src/main/java/poitevie/coopcycle/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package poitevie.coopcycle.repository;
-
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
-import poitevie.coopcycle.domain.Authority;
-
-/**
- * Spring Data R2DBC repository for the {@link Authority} entity.
- */
-public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
